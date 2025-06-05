dcc71f15d28bdaf6892e74405eacf3aebabd0cfa
macmade <macmade@xs-labs.com>
2025-06-05 18:34:49 +0200

    feat: Stretching

--- SwiftPixel/Processors/Stretch.swift
+++ SwiftPixel/Processors/Stretch.swift
@@ -22,7 +22,9 @@
  * THE SOFTWARE.
  ******************************************************************************/
 
+import Accelerate
 import Foundation
+import SwiftUtilities
 
 public extension Processors
 {
@@ -53,6 +55,84 @@ public extension Processors
         }
 
         public func process( buffer: inout PixelBuffer ) throws
-        {}
+        {
+            guard buffer.isNormalized
+            else
+            {
+                throw RuntimeError( message: "Buffer needs to be normalized" )
+            }
+
+            switch self.algorithm
+            {
+                case .log(     let n ):          try Self.logStretch(     buffer: &buffer, n: n )
+                case .arcsinh( let n ):          try Self.arcsinhStretch( buffer: &buffer, n: n )
+                case .sigmoid( let n1, let n2 ): try Self.sigmoidStretch( buffer: &buffer, n1: n1, n2: n2 )
+            }
+        }
+
+        private static func logStretch( buffer: inout PixelBuffer, n: Double ) throws
+        {
+            var denominator = log( 1.0 + n )
+            var one         = 1.0
+            let count       = vDSP_Length( buffer.pixels.count )
+
+            try buffer.pixels.withUnsafeMutableBufferPointer
+            {
+                guard let baseAddress = $0.baseAddress
+                else
+                {
+                    throw RuntimeError( message: "Failed to access data buffer" )
+                }
+
+                vDSP_vsmulD( baseAddress, 1, [ n ], baseAddress, 1, count )
+                vDSP_vsaddD( baseAddress, 1, &one, baseAddress, 1, count )
+                vvlog( baseAddress, baseAddress, [ Int32( count ) ] )
+                vDSP_vsdivD( baseAddress, 1, &denominator, baseAddress, 1, count )
+            }
+        }
+
+        private static func arcsinhStretch( buffer: inout PixelBuffer, n: Double ) throws
+        {
+            var denominator = asinh( n )
+            let count       = vDSP_Length( buffer.pixels.count )
+
+            try buffer.pixels.withUnsafeMutableBufferPointer
+            {
+                guard let baseAddress = $0.baseAddress
+                else
+                {
+                    throw RuntimeError( message: "Failed to access data buffer" )
+                }
+
+                vDSP_vsmulD( baseAddress, 1, [ n ], baseAddress, 1, count )
+                vvasinh( baseAddress, baseAddress, [ Int32( count ) ] )
+                vDSP_vsdivD( baseAddress, 1, &denominator, baseAddress, 1, count )
+            }
+        }
+
+        private static func sigmoidStretch( buffer: inout PixelBuffer, n1: Double, n2: Double ) throws
+        {
+            let count    = vDSP_Length( buffer.pixels.count )
+            var midpoint = -n2
+            var slope    = n1
+            var minusOne = -1.0
+            var one      = 1.0
+
+            try buffer.pixels.withUnsafeMutableBufferPointer
+            {
+                guard let baseAddress = $0.baseAddress
+                else
+                {
+                    throw RuntimeError( message: "Failed to access data buffer" )
+                }
+
+                vDSP_vsaddD( baseAddress, 1, &midpoint, baseAddress, 1, count )
+                vDSP_vsmulD( baseAddress, 1, &slope, baseAddress, 1, count )
+                vDSP_vsmulD( baseAddress, 1, &minusOne, baseAddress, 1, count )
+                vvexp( baseAddress, baseAddress, [ Int32( count ) ] )
+                vDSP_vsaddD( baseAddress, 1, &one, baseAddress, 1, count )
+                vvrec( baseAddress, baseAddress, [ Int32( count ) ] )
+            }
+        }
     }
 }
